68d093c869615c84f26acc768cfca12bd28f2576
davleft <davleftvaz99@gmail.com>
2021-12-30 01:51:47 -0500

    cambiolol

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -13,7 +13,7 @@ const nuevoBool = true;
 const nuevaResta = 10 - 5 === 5;
 
 // Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = 10 * 4 === 40 ;
+const nuevaMultiplicacion = 10 * 4 === 40;
 
 // Resuelve el siguiente problema matemático:tu
 const nuevoModulo = 21 % 5 === 1;
@@ -132,13 +132,13 @@ function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
-  return num*num;
+  return Math.pow(num,2);
 }
 
 function elevarAlCubo(num) {
   // Devuelve el valor de "num" elevado al cubo
   // Tu código:
-  return num*num*num;
+  return Math.pow(num,3);
 }
 
 function elevar(num, exponent) {
@@ -173,9 +173,9 @@ function esPositivo(numero) {
   if(numero === 0){
     return false;
   }else if(numero > 0) {
-    return "Es Positivo";
+    return "Es positivo";
   }else {
-    return "Es Negativo";
+    return "Es negativo";
   }
 }
 
@@ -183,21 +183,22 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
-  return "hello world" + "!"
+  return str + "!";
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  return "nombre" + "" + "apellido"; 
+  var com = nombre + ' ' + apellido;
+  return com; 
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return "Hola" + "Martin";
+  return "Hola " + nombre +"!";
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
@@ -225,7 +226,7 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  
+  return euro*1.20;
 }
 
 
@@ -235,7 +236,13 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
+  if (letra.length>1){
+    return "Dato incorrecto";
+  }else if(letra === "a"||letra ==="e"||letra === "i"||letra ==="o"||letra === "u"){
+    return "Es vocal";
+  }else {
+    return "Dato incorrecto";
+  }
 }
 
 
